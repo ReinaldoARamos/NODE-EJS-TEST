3c3161f140e25b2c173aa0d0e859326044630ec0
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-12-01 23:07:36 -0300

    passamos o set para o express entender que iremos usar ejs para renderizar o HTML, além disso, foi criado a rota / usando o .get

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express = require('express'); //chamando a biblioteca express
 
+
+const app = express(); //criando o const app para aplicar a biblioteca express
+
+app.set("view engine", "ejs" ); //passamos para o express que o ejs será usado para renderizar o HTML
+
+app.get("/", function(req, res) {
+    res.render("index")
+})
