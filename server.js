2d78cda40f2a143cdb12805953e26ba9af106fb5
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-12-01 23:22:55 -0300

    criando o servidor que irá renderizar os ejs pela rota usando express, e criando a pasta view onde o express irá renderizar os mesmos

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express(); //criando o const app para aplicar a biblioteca express
 
 app.set("view engine", "ejs" ); //passamos para o express que o ejs será usado para renderizar o HTML
 
-app.get("/", function(req, res) {
+app.get("/", function(req, res) { //criando a rota com o .get
     res.render("index")
 })
+
+app.get("/about", function(req, res) { //criando a rota sobre com o .get
+    res.render("about")
+})
+
+app.listen(8080); //função do express que coloca ele pra ouvir a prota 8080, ou seja, ele ta roddando na máquina a porta 8080
+console.log("rodando")
