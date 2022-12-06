f080ec5a6647b824ac5d6d5c25afc2e2ca425cb2
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-12-05 22:18:40 -0300

    refatorando o código e separando em pastas separadas, além disso, organizando a chamada dos elementos

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express(); //criando o const app para aplicar a biblioteca express
 app.set("view engine", "ejs" ); //passamos para o express que o ejs será usado para renderizar o HTML
 
 app.get("/", function(req, res) { //criando a rota com o .get
-    res.render("index")
+    res.render("pages/index")
 })
 
 app.get("/about", function(req, res) { //criando a rota sobre com o .get
-    res.render("about")
+    res.render("pages/about")
 })
 
 app.listen(8080); //função do express que coloca ele pra ouvir a prota 8080, ou seja, ele ta roddando na máquina a porta 8080
