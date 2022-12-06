a04b988ad858148041977b50e1f7531692c15373
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-12-05 23:06:24 -0300

    criando uma lista com algumas mensagens escritas

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express(); //criando o const app para aplicar a biblioteca express
 app.set("view engine", "ejs" ); //passamos para o express que o ejs será usado para renderizar o HTML
 
 app.get("/", function(req, res) { //criando a rota com o .get
+    const items = [
+        {title: "D", Message: "Di moruto"},
+        {title: "A", Message: "Arrivederci"},
+        {title: "F", Message: "Fingaaaaar"},
+        {title: "E", Message: "EMERALDO SPURACH"}
+    ]
     res.render("pages/index")
 })
 
